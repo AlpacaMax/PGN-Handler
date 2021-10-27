879149f8b6ab13f0344e2d988f89b422e00b0ba3
Maxwell Yang <31149339+AlpacaMax@users.noreply.github.com>
2021-10-27 00:59:02 -0400

    Change from chess to chess.js

--- pgn_validator.js
+++ pgn_validator.js
@@ -1,53 +1,53 @@
-const chess = require('chess');
+const { Chess } = require('chess.js');
 const _ = require('lodash');
 
 function validate(parsedPgn) {
-    function _helper(moves, gameClient) {
-        let moveObjs = [];
+    function _helper(moves, chess) {
+        let numOfHalfMoves = 0;
 
         for (let turn of moves) {
             // Validation for white move
             if (turn.white != null) {
-                let movesForWhite = gameClient.getStatus().notatedMoves;
-                if (!(turn.white.SAN in movesForWhite)) {
+                let movesForWhite = chess.moves();
+                if (!movesForWhite.includes(turn.white.SAN)) {
                     turn.white.LegalMove = false;
                     break;
                 }
                 turn.white.LegalMove = true;
                 if ("RAV" in turn.white) {
-                    _helper(turn.white.RAV, gameClient);
+                    _helper(turn.white.RAV, chess);
                 }
-                let m = gameClient.move(turn.white.SAN);
-                turn.white.From.File = m.move.prevSquare.file;
-                turn.white.From.Rank = m.move.prevSquare.rank;
-                moveObjs.push(m);
+                let m = chess.move(turn.white.SAN);
+                turn.white.From.File = m.from[0];
+                turn.white.From.Rank = m.from[1];
+                ++numOfHalfMoves;
             }
 
             // Validation for black move
             if (turn.black != null) {
-                let movesForBlack = gameClient.getStatus().notatedMoves;
-                if (!(turn.black.SAN in movesForBlack)) {
+                let movesForBlack = chess.moves();
+                if (!movesForBlack.includes(turn.black.SAN)) {
                     turn.black.LegalMove = false;
                     break;
                 }
                 turn.black.LegalMove = true;
                 if ("RAV" in turn.black) {
-                    _helper(turn.black.RAV, gameClient);
+                    _helper(turn.black.RAV, chess);
                 }
-                let m = gameClient.move(turn.black.SAN);
-                turn.black.From.File = m.move.prevSquare.file;
-                turn.black.From.Rank = m.move.prevSquare.rank;
-                moveObjs.push(m);
+                let m = chess.move(turn.black.SAN);
+                turn.black.From.File = m.from[0];
+                turn.black.From.Rank = m.from[1];
+                ++numOfHalfMoves;
             }
         }
 
-        for (let i = moveObjs.length - 1; i >= 0; --i) {
-            moveObjs[i].undo();
+        for (let i = 0; i < numOfHalfMoves; ++i) {
+            chess.undo();
         }
     }
 
-    const gameClient = chess.create({ PGN: true });
-    _helper(parsedPgn.Moves, gameClient);
+    const chess = new Chess();
+    _helper(parsedPgn.Moves, chess);
 }
 
 exports.validate = validate;
