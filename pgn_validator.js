01ffe3cf8cee943489ee2cd616c80a5ae0b826d4
Maxwell Yang <31149339+AlpacaMax@users.noreply.github.com>
2021-10-26 23:57:44 -0400

    FIX Deep Clone Bug

--- pgn_validator.js
+++ pgn_validator.js
@@ -3,6 +3,8 @@ const _ = require('lodash');
 
 function validate(parsedPgn) {
     function _helper(moves, gameClient) {
+        let moveObjs = [];
+
         for (let turn of moves) {
             // Validation for white move
             if (turn.white != null) {
@@ -13,11 +15,12 @@ function validate(parsedPgn) {
                 }
                 turn.white.LegalMove = true;
                 if ("RAV" in turn.white) {
-                    _helper(turn.white.RAV, _.cloneDeep(gameClient));
+                    _helper(turn.white.RAV, gameClient);
                 }
                 let m = gameClient.move(turn.white.SAN);
                 turn.white.From.File = m.move.prevSquare.file;
                 turn.white.From.Rank = m.move.prevSquare.rank;
+                moveObjs.push(m);
             }
 
             // Validation for black move
@@ -29,17 +32,21 @@ function validate(parsedPgn) {
                 }
                 turn.black.LegalMove = true;
                 if ("RAV" in turn.black) {
-                    _helper(turn.black.RAV, _.cloneDeep(gameClient));
+                    _helper(turn.black.RAV, gameClient);
                 }
-                // console.log(turn.black.SAN);
                 let m = gameClient.move(turn.black.SAN);
                 turn.black.From.File = m.move.prevSquare.file;
                 turn.black.From.Rank = m.move.prevSquare.rank;
+                moveObjs.push(m);
             }
+        }
+
+        for (let i = moveObjs.length - 1; i >= 0; --i) {
+            moveObjs[i].undo();
         }
     }
 
-    const gameClient = chess.create({ PGN: true});
+    const gameClient = chess.create({ PGN: true });
     _helper(parsedPgn.Moves, gameClient);
 }
 
