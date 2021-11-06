77720aaa4abbac262a5787afca2021a382efd6c4
Maxwell Yang <31149339+AlpacaMax@users.noreply.github.com>
2021-11-05 22:39:18 -0400

    Add test for move types

--- pgn_handler.test.js
+++ pgn_handler.test.js
@@ -9,3 +9,14 @@ test('Test legal move validation', () => {
     expect(parsedPgn.moves[0].black.rav[0].black.legalMove).toBeFalsy();
     expect(parsedPgn.moves[1].white.legalMove).toBeTruthy();
 });
+
+test('Test move types', () => {
+    let parsedPgn = parser.parse("test_pgns/test_move_type.pgn");
+    expect(parsedPgn.moves[0].white.type).toBe(1);
+    expect(parsedPgn.moves[0].black.type).toBe(3);
+    expect(parsedPgn.moves[1].white.type).toBe(4);
+    expect(parsedPgn.moves[1].black.type).toBe(8);
+    expect(parsedPgn.moves[2].white.type).toBe(17);
+    expect(parsedPgn.moves[2].black.type).toBe(33);
+    expect(parsedPgn.moves[3].white.type).toBe(65);
+})
